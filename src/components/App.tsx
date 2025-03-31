e3d867f3f2beef93e1081cd99ddc3a12ce9e3fe5
sammy-may <samuel.james.may1@gmail.com>
2025-03-30 18:04:54 -0700

    :tada: basic vite react + ts + tailwindcss setup

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="bg-gray-500 text-red-500 flex items-center">
+            <p>My personal website</p>
+        </div>
+    );
+}
+
+export default App;
